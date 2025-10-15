3a45b1950caf0b927a586c7d88e84c5ac0409f94
PabbisettySivaKumar <pabbisettyssivakumar@gmail.com>
2025-10-15 19:48:34 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-
-embedder = OllamaEmbeddings(model="nomic-embed-text:latest")
-vec = embedder.embed_query("Testing Ollama embeddings")
-print(len(vec))
